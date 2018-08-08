39ecb9a63b87caa1d2ca9c7db0dd74aaa21ca715
Nimrod <nimrodbens@gmail.com>
2018-08-08 14:49:55 +0300

    ...

--- UIViewAutoLayoutExtension.swift
+++ UIViewAutoLayoutExtension.swift
@@ -2,8 +2,8 @@
 //  UIViewAutoLayoutExtension.swift
 //  AutolayoutDemo
 //
-//  Created by Stephen Yao on 8/07/2015.
-//  Copyright (c) 2015 SilverBear. All rights reserved.
+//  Created by Nimrod Ben Simon on 8/07/2016.
+//  Copyright (c) 2016 Nissim. All rights reserved.
 //
 import UIKit
 
